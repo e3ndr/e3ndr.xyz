d4fdda44f1eca6f74b7d10cd0456cf40a23c83ad
Ender <33337309+e3ndr@users.noreply.github.com>
2022-05-25 14:56:45 -0500

    Update SvelteKit.

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter()
 	}
 };
 
