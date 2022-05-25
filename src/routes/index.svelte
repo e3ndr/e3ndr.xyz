5c54aab78a4520b928677daa09e7c23ab98c74dc
Ender <33337309+e3ndr@users.noreply.github.com>
2022-05-25 15:16:04 -0500

    Add a footer and "Profile Pictures"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,8 +23,7 @@
             <a href="https://github.com/Casterlabs/Rakurai/tree/main/Json" target="_blank">Rson</a>, a Json library for Java, because Gson and Jackson were pissing me off.
         </li>
         <li>
-            <a href="https://emoji.casterlabs.co" target="_blank">Emoji for everyone</a>, a handy-dandy helper (like Twemoji) that allows you to use different Emoji providers
-            easily.
+            <a href="https://emoji.casterlabs.co" target="_blank">Emoji for everyone</a>, a handy-dandy helper (like Twemoji) that allows you to use different Emoji providers easily.
         </li>
         <li>
             <a href="https://github.com/Casterlabs/Kaimen" target="_blank">Kaimen</a>, a Java library for building desktop apps that use Webviews for their UI. Similar to Electron.
@@ -33,17 +32,13 @@
 
     <br />
 
-    <p>
-        As you can probably tell, I'm well versed in Java (11 is preferred, but 8 just feels like home). When it comes to web development, I prefer the Svelte + custom Java API
-        approach.
-    </p>
+    <p>As you can probably tell, I'm well versed in Java (11 is preferred, but 8 just feels like home). When it comes to web development, I prefer the Svelte + custom Java API approach.</p>
     <p>I'm pretty handy with HTML + CSS, and wouldn't mind the occasional work, but I prefer the backend more than anything.</p>
 
     <br />
 
     <p>
-        So, if you're interested in getting in touch, you can do so by emailing me at <a href="mailto:balexbowles@gmail.com">balexbowles@gmail.com</a>. If you're too hip for email
-        you can always reach me on my <a href="https://twitter.com/ItzLcyx">Twitter</a>, or even pester me on Discord
+        So, if you're interested in getting in touch, you can do so by emailing me at <a href="mailto:balexbowles@gmail.com">balexbowles@gmail.com</a>. If you're too hip for email you can always reach me on my <a href="https://twitter.com/ItzLcyx">Twitter</a>, or even pester me on Discord
         <span>
             (
             <pre>Ɇ₦ĐɆⱤV₳₥₱łⱤɆ#9092</pre>
@@ -62,6 +57,16 @@
             <option value="openmoji">Openmoji</option>
             <!-- <option value="sensa-emoji">Sensa Emoji</option> -->
         </select>
+    </span>
+
+    <br />
+    <br />
+
+    <span>
+        <a href="https://twitter.com/ItzLcyx">Twitter</a> &bull;
+        <a href="https://github.com/e3ndr">Github</a> &bull;
+        <a href="https://instagram.com/ItzLcyx">Instagram</a> &bull;
+        <a href="/ahegao.zip">"Profile Pictures"</a>
     </span>
 </section>
 
