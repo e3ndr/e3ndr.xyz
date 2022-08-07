9e563d6404fd8345a34059f8786186b203fdf539
Ender <33337309+e3ndr@users.noreply.github.com>
2022-08-06 20:55:20 -0500

    Attempt to target IE 11.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import legacy from '@vitejs/plugin-legacy';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()]
+    plugins: [
+        sveltekit(),
+        legacy({
+            targets: ['defaults', 'ie >= 11'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+        })
+    ]
 };
 
 export default config;
