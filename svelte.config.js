31c85d4adb43148341a962d83140ea5bf9510db9
Ender <33337309+e3ndr@users.noreply.github.com>
2022-08-06 20:24:14 -0500

    Update SvelteKit.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		prerender: { default: true},
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
