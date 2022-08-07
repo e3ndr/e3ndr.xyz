75655130c10e223c1d20db873c827cafef3d87f8
Ender <33337309+e3ndr@users.noreply.github.com>
2022-08-06 21:11:00 -0500

    plugin-legacy didn't work.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import legacy from '@vitejs/plugin-legacy';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [
-        sveltekit(),
-        legacy({
-            targets: ['defaults', 'ie >= 11'],
-            additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-        })
-    ]
+    plugins: [sveltekit()]
 };
 
 export default config;
