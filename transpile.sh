9084c5dc2c97c069b190d2f430a1cc1b81118c42
Ender <33337309+e3ndr@users.noreply.github.com>
2022-08-07 04:14:52 -0500

    Getting somewhere.

--- transpile.sh
+++ transpile.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# rm -rf build/compiled
+# mkdir build/raw
+# mkdir build/compiled
+# mv build/* build/raw
+
+npx babel build/raw --config-file ./babel.config.json --out-dir build/compiled --copy-files
