83d9c8837e0d923c65ebef40e75a6473900540bb
Ender <33337309+e3ndr@users.noreply.github.com>
2023-01-04 23:58:13 -0600

    Update the site, get ready for big tingz!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import CSSIntermediate from '$lib/CSSIntermediate.svelte';
+	import Navbar from '$lib/layout/Navbar.svelte';
+	import Footer from '$lib/layout/Footer.svelte';
+</script>
+
+<CSSIntermediate>
+	<div class="flex flex-col min-h-full">
+		<!-- Here so the layout will work properly. -->
+		<Navbar />
+
+		<!-- This'll grow to fill any remaining space. It'll also overflow the container if needed. -->
+		<main class="flex-grow">
+			<slot />
+		</main>
+	</div>
+
+	<!-- Here so the footer will get pushed off screen. -->
+	<Footer />
+</CSSIntermediate>
