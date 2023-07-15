c13c98833c8a28611d1fb27ec6126a565294c29b
Ender <33337309+e3ndr@users.noreply.github.com>
2023-07-15 17:48:28 -0500

    Sort everything out to fix BipBop.

---
+++ src/routes/(plain)/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { onMount } from 'svelte';
+	import { browser, dev } from '$app/environment';
+	import { page } from '$app/stores';
+	import * as Swetrix from 'swetrix';
+
+	onMount(() => {
+		Swetrix.init('nfebI15q8Nfk', { debug: dev, disabled: dev });
+		Swetrix.trackViews();
+	});
+
+	// Trigger a pageview everytime the user navigates.
+	// We use the `browser` env variable to ensure that we don't try to
+	// call trackViews while the component is doing SSR.
+	$: $page.url.pathname, browser && Swetrix.trackViews();
+</script>
+
+<slot />
