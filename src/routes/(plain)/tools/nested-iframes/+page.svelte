264a7e20a4b48b69e9b5524f1da6f31f1e4becde
e3ndr <33337309+e3ndr@users.noreply.github.com>
2026-09-02 02:44:47 -0500

    fresh slate.

--- src/routes/(plain)/tools/nested-iframes/+page.svelte
+++
@@ -1,5 +0,0 @@
-Root
-<br />
-<iframe
-	src="data:text/html;charset=utf-8,%3C%21DOCTYPE%20html%3E%0D%0A%3Chtml%3E%0D%0Aone%20deep%0D%0A%3Ciframe%20src%3D%22data%3Atext%2Fhtml%3Bcharset%3Dutf-8%2Ctwo%20deep%22%20%2F%3E%0D%0A%3C%2Fhtml%3E"
-/>
